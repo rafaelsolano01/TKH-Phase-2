24fad06b2b5b1fb3475750c4580ac8b70843968a
Rafael Solano <ralph_piano@yahoo.com>
2026-08-25 18:53:08 +0000

    Fix variable name mismatch to my_home_ip

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "home_ip" {
+variable "my_home_ip" {
   description = "Specific home IP address for SSH access (CIDR notation)"
   type        = string
   default     = "24.146.146.222/32"
